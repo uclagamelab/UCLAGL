63f6aa2a4d3ad116ea24641bbf79a5616783aa97
David Elliott <david@games.ucla.edu>
2011-04-21 12:17:55 -0700

    added interview option for resource type

--- custom/resource_type_meta.php
+++ custom/resource_type_meta.php
@@ -3,6 +3,7 @@
 	<select name="<?php $mb->the_name(); ?>">
 		<option value="">Select...</option>
 		<option value="article"<?php $mb->the_select_state('article'); ?>>Article</option>
+		<option value="interview"<?php $mb->the_select_state('interview'); ?>>Interview</option>
 		<option value="lecture"<?php $mb->the_select_state('lecture'); ?>>Lecture</option>
 		<option value="show"<?php $mb->the_select_state('show'); ?>>Show</option>
 		<option value="workshop"<?php $mb->the_select_state('workshop'); ?>>Workshop</option>
