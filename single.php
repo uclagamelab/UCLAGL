f28d2f79881156f6af9416926f2022c2df55cfc8
David Elliott <david@games.ucla.edu>
2011-11-17 16:02:36 -0800

    fixed featured image size problem, finished single.php next need to update the typography for all text from adelines specs

--- single.php
+++ single.php
@@ -1,52 +1,28 @@
 <?php get_header(); ?>
-
+<section id="main_section">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<section>
+		<section id="post_main">
 			<article id="post-<?php the_ID(); ?>">
-				<header>
-					<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+
+				<?php
+				echo('<div id="opening_image" >');
+				the_post_thumbnail('slider'); 
+				echo('</div>');
+				?>
+				<header>	
+					<h1><?php the_title(); ?></h1>	
 					<p>Posted on <?php the_time('F jS, Y'); ?> by <?php the_author(); ?></p>
 				</header>
-				<section>
-					<?php the_content('Read more on "'.the_title('', '', false).'" &raquo;'); ?>
 
-				</section>
-				<footer>
-					<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-					<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
 
-					<p>
-						This entry was posted on <?php the_time('l, F jS, Y'); ?> at <?php the_time(); ?> and is filed under <?php the_category(', ') ?>. 
-						You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
+					
 
-						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// both comments and pings open ?>
-							You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
+					
+					<?php the_content(); ?>
 
-						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// only pings are open ?>
-							Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.
-
-						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// comments are open, pings are not ?>
-							You can skip to the end and leave a response. Pinging is currently not allowed.
-
-						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// neither comments nor pings are open ?>
-							Both comments and pings are currently closed.
-
-						<?php } edit_post_link('Edit this entry','','.'); ?>
-
-					</p>
-				</footer>
 			</article>
 
-			<?php comments_template(); ?>
-
-			<nav>
-				<p><?php previous_post_link(); ?> &bull; <?php next_post_link(); ?></p>
-			</nav>
 		</section>
 
 	<?php endwhile; else: ?>
@@ -59,6 +35,6 @@
 
 	<?php endif; ?>
 
-<?php get_sidebar(); ?>
+</section><!-- main_section -->
 
 <?php get_footer(); ?>
