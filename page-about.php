e997310e4e63da9ea66c496b5d188866458b9e1c
David Elliott <david@games.ucla.edu>
2011-11-23 15:06:35 -0800

    added map to about page

--- page-about.php
+++ page-about.php
@@ -23,10 +23,13 @@ Template Name: About Page
 				<?php the_content(); ?>
 			</section><!-- main_description -->
 			
+			
 			<?php endwhile; ?>
 			
+			<section id="about_map">
+				<iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=ucla+game+lab&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=78.693042,63.632813&amp;vpsrc=6&amp;ie=UTF8&amp;hq=ucla+game+lab&amp;hnear=&amp;t=m&amp;ll=34.076933,-118.440557&amp;spn=0.006221,0.01929&amp;z=16&amp;output=embed"></iframe>
+			</section> <!--end of map-->
 			
-
 			
 			<section id="contact" class="embed">
 				<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('contact') ) : endif; ?>
